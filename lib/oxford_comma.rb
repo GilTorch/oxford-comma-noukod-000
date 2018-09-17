f889afea5c3dc2741ada9db7132f6cd10609a890
Gilbert Torchon <gilbert@noukod.com>
2018-09-17 21:07:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   last_element= ""
-  if array.size > 3
-    last_element = array.pop
-    array=array.join(", ") << " and "+last_element
+  if array.size == 1
+  elsif array.size == 2
+  elsif array.size == 3 
   end
   array
 end
