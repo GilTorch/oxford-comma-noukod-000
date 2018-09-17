cff132a7dc09cd234cb37154cd54a38c936bfcca
Gilbert Torchon <gilbert@noukod.com>
2018-09-17 21:05:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   last_element= ""
   if array.size > 3
     last_element = array.pop
+    array=array.join(", ") << " and "+last_element
   end
-  array.join(", ") << " and "+last_element
+  
 end
